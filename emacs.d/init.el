856e1f83f7ade6c0b4a92656c5ae193bdbf1fce5
Paul Geringer <paul.j.geringer@gmail.com>
2014-11-21 19:43:16 -0600

    Add in init.el and stop emacs swp

---
+++ emacs.d/init.el
@@ -0,0 +1,21 @@
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(require 'package)
+(package-initialize)
+
+(menu-bar-mode -1)
+
+(require 'evil)
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(evil-mode 1)
