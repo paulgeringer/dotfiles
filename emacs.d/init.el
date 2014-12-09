d579e9d09d0e611deb147c2653f33ce79b88f2c7
Paul Geringer <paul.j.geringer@gmail.com>
2014-12-08 22:59:03 -0600

    update emacsy stuff and some junk

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
@@ -13,15 +13,17 @@
 
 (require 'evil)
 (require 'evil-leader)
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+;;(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+;;(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+;;(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+;;(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+(evil-define-key 'normal org-mode-map (kbd "TAB") #'org-cycle)
 
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(auto-fill-mode 1) ;; this might break stuff
 
 (global-evil-leader-mode)
 (evil-mode 1)
@@ -31,3 +33,4 @@
 (load-file "~/.emacs.d/color-theme-tomorrow.el")
 (require 'color-theme-tomorrow)
 (color-theme-tomorrow--define-theme night)
+(require 'helm-config)
