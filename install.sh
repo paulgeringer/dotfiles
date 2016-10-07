465f971a5afe1e141ad331b212ec571c2077a636
Paul Geringer <paul.j.geringer@gmail.com>
2016-10-06 19:02:19 -0500

    Fix something that was horrifically broken on my home laptop and make some small typo adjustments and fixes

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function brew_packages() {
-  if type brew;
+  if type brew > /dev/null 2>&1;
   then
     brew tap thoughtbot/formulae
-    brew install "$(cat brew_leaves)"
+    brew install $(cat brew_leaves)
   else
     echo "Install Homebrew first."
     exit 1
@@ -12,7 +12,7 @@ function brew_packages() {
 }
 
 function symlink_dotfiles() {
-  if type rcup && [ -d ~/dotfiles ];
+  if type rcup > /dev/null 2>&1 && [ -d ~/dotfiles ];
   then
     rcup -x install.sh -x com.googlecode.iterm2.plist -x brew_leaves # initial set up, so assume .rcrc isn't installed yet
   else
@@ -24,13 +24,13 @@ function symlink_dotfiles() {
 function setup_vundle_tpm() {
   VUNDLEDIR=$HOME/.vim/bundle/Vundle.vim
 
-  if [ ! -e "$VUNDLEDIR" ];
+  if [ ! -d "$VUNDLEDIR" ];
   then
     git clone https://github.com/gmarik/vundle.git "$VUNDLEDIR"
     vim +PluginInstall +qa
   fi
 
-  if [ ! -e "$HOME/.tmux/plugins/tpm" ];
+  if [ ! -d "$HOME/.tmux/plugins/tpm" ];
   then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   fi
@@ -38,4 +38,4 @@ function setup_vundle_tpm() {
 
 brew_packages
 symlink_dotfiles
-setup_vundle
+setup_vundle_tpm
